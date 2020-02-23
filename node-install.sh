c787431dc9c92967fcdd5272933af92616647f01
Dennis Ying <dennisyingswebpages@gmail.com>
2020-02-23 19:45:52 +1100

    Threading to improve docker swarm efficiency (#3)
    -   Updated node install script (docker swarm join may be buggy)
    -   Improved threading performance (non-blocking)
    -   Format program output
    -   Refactored file search to allow nested folders (generator)
    -   Refactored node search to preemptively wake shutdown workers (generator) (untested)

--- node-install.sh
+++ node-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+NFS_IP="192.168.0.150"
+DOCKER_WORKER_JOIN_TOKEN="SWMTKN-1-26k9pgeq0twp9cd8xzoa188o0pni1dgpsb18j78vrhzobdkigm-d24afnfa192ferahx0zzmu73i"
+# default port for docker 
+DOCKER_SWARM_PORT="2377"
+
 # Check for root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -8,7 +13,7 @@ fi
 
 # Update packages
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 # Install docker using convenience script
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -23,8 +28,15 @@ sudo apt-get install nfs-common -y
 # Create mount point for NFS
 sudo mkdir -p /mnt/nfs-ffmpeg
 
-# Prompt for server IP
 # save persistent mount in /etc/fstab
+echo "$NFS_IP:/mnt/nfs-ffmpeg /mnt/nfs-ffmpeg nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" | sudo tee -a /etc/fstab
+
 # Add to docker swarm
 # sudo needed as changed to user group not effective as no reboot
+sudo docker swarm --token $DOCKER_WORKER_JOIN_TOKEN $NFS_IP:$DOCKER_SWARM_IP
+
+# Allow all users to shutdown and reboot without password
+echo "ALL ALL=NOPASSWD: /sbin/reboot, /sbin/shutdown, /sbin/poweroff" | sudo tee -a /etc/sudoers
+
 # reboot
+sudo reboot
--- scheduler/nodes.py
+++ scheduler/nodes.py
@@ -1,8 +1,9 @@
 from wol import send_magic_packet
-from global_var import node_list
+from global_var import node_list, NODE_PENDING_INTERVAL, NODE_UPDATE_INTERVAL, status_header
 import subprocess
 import json
 from time import sleep
+from datetime import datetime
 
 def wake_on_lan(mac_addr):
     send_magic_packet(mac_addr)
@@ -19,11 +20,14 @@ class DockerNode:
         self.hostname = hostname
         self.online = False
         self.busy = False
+        self.pending = False
+        self.pending_datetime = datetime.now().timestamp()
         self.availability = "active"
         self.ip = "0.0.0.0"
         DockerNode.instances.append(self)
 
     def update_busy(self):
+        initial_status = self.busy
         # Insecure to run pipes
         # Run commands individually
         # https://stackoverflow.com/questions/13332268/how-to-use-subprocess-command-with-pipes
@@ -39,8 +43,22 @@ class DockerNode:
         
         ffmpeg_container_count = int(count_lines.stdout.decode('utf-8').strip())
         self.busy = ffmpeg_container_count > 0
+        final_status = self.busy
+        
+        if self.busy:
+            self.pending = False
+        elif datetime.now().timestamp() - self.pending_datetime > NODE_PENDING_INTERVAL:
+            self.pending = False 
+
+        if initial_status == True and final_status == False:
+            print(f"{status_header(self.hostname)}No longer busy")
 
         return ffmpeg_container_count
+
+
+    def pending_busy(self):
+        self.pending = True
+        self.pending_datetime = datetime.now().timestamp()
 
 
     def update_status(self):
@@ -58,38 +76,84 @@ class DockerNode:
             self.update_busy()
         else: # Down
             self.busy = False
+            self.pending = False
             self.online = False
 
         
-
 def get_free_node():
-    offline_nodes = []
     while True:
+        offline_nodes = []
         for node in DockerNode.instances:
             node.update_status()
-            if node.online and not node.busy:
-                return node.hostname
+            if node.online and not node.busy and not node.pending:
+                node.pending_busy()
+                yield node.hostname
             elif not node.online:
+                # Pre-emptively wake nodes as tasks usually come in batches
+                wake_on_lan(node.mac_addr)
                 offline_nodes.append(node)
-        for i in range(0, len(offline_nodes)):
-            print(f"Waking up worker with hostname '{offline_nodes[i].hostname}'.")
-            wake_on_lan(offline_nodes[i].mac_addr)
+        
+        # Handle previously offline nodes
+        while len(offline_nodes) > 0:
             tries = 0
             # Wait for up to a minute for worker node to boot and reconnect to swarm
+            # If dead worker due to very slow startup or hardware failure, ignore and move on
             while tries < 12:
-                offline_nodes[i].update_status()
-                if offline_nodes[i].online:
-                    print(f"Worker with hostname '{offline_nodes[i].hostname}' woke up!")
-                    return offline_nodes[i].hostname
+                for i in range(0, len(offline_nodes)):
+                    wake_on_lan(offline_nodes[i].mac_addr)
+                    offline_nodes[i].update_status()
+                    if offline_nodes[i].online:
+                        node = offline_nodes.pop(i)
+                        node.pending_busy()
+                        yield node.hostname
+                        tries = 12345
+                        break
                 sleep(5)
                 tries += 1
+            # Waited for a long time but these nodes still offline
+            # Probably dead or requiring human interaction
+            if tries != 12345:
+                for node in offline_nodes:
+                    print(f"{status_header('ERROR')}Can't wake {node.hostname}")
+                    DockerNode.instances.remove(node)
+                offline_nodes = []
+            
+                
+                
+        # More than sufficient time for any previously scheduled 
+        # docker services to start and converge
+        wait_start = datetime.now().timestamp()
+        print("Waiting on busy nodes to free up...", end='\r', flush=True)
+        all_busy = True
+        while all_busy:
+            sleep(NODE_UPDATE_INTERVAL)
+            elapsed_time = int(datetime.now().timestamp() - wait_start)
+            print(f"Waiting on busy nodes to free up...\t{elapsed_time} s", end='\r', flush=True)
+            for node in DockerNode.instances:
+                # if offline
+                if not node.online:
+                    wake_on_lan(node.mac_addr)
+                # if online and no tasks
+                elif not node.busy and not node.pending:
+                    all_busy = False
+                    break
+
+
+def node_by_hostname(hostname):
+    for node in DockerNode.instances:
+        if node.hostname == hostname:
+            return node
+
 
 def initialise_nodes():
     for mac_addr, hostname in node_list:
         DockerNode(mac_addr, hostname)
     for node in DockerNode.instances:
-        print(f"Updating:\t\t{node.mac_addr}\t{node.hostname}")
-        node.update_status()
-    
-
+        print(f"{status_header('INITIALISATION')}MAC: {node.mac_addr}\tHOSTNAME: {node.hostname}")
         
+
+def poll_node_status():
+    while True:
+        for node in DockerNode.instances:
+            node.update_status()
+        sleep(NODE_UPDATE_INTERVAL)
